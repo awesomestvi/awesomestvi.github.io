aeec1ab77e0bda03f207d60eca922f7c51ceab8a
awesomestvi <vishal.cn.92@gmail.com>
2018-02-06 04:33:28 +0530

    Added Magic Square

--- magicsquare.js
+++ magicsquare.js
@@ -1,66 +1,99 @@
 document.body.style.backgroundColor = "#333";
 document.body.style.color = "#fff";
 
-var arr = [
-    [2, 7, 6],
-    [9, 5, 1],
-    [4, 3, 8]
-];
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
 
-// Initialize value
+document.write('<input id="matrixSize1" type="text"/>');
+document.write('<input id="matrixSize2" type="text"/>');
+document.write('<input id="matrixSize3" type="text"/>');
+document.write("<br>");
+document.write('<input id="matrixSize4" type="text"/>');
+document.write('<input id="matrixSize5" type="text"/>');
+document.write('<input id="matrixSize6" type="text"/>');
+document.write("<br>");
+document.write('<input id="matrixSize7" type="text"/>');
+document.write('<input id="matrixSize8" type="text"/>');
+document.write('<input id="matrixSize9" type="text"/>');
+document.write("<br>");
+document.write('<button onclick="go()" type="submit">Go</button>');
+
+var doc = {};
+var array = [];
+
 var sum = 0;
 var magicSum = 0;
 var isMagic = true;
+var result = "";
 
-for(r = 0; r < arr.length; r++) {
-    magicSum += arr[0][r];
-}
+function go() {
+    
+    for(i = 1; i < 10; i++){
+        doc["a" +i+ ""] = parseInt(document.getElementById('matrixSize'+i).value);
+    }
+    
+    array = [
+        [doc.a1, doc.a2, doc.a3],
+        [doc.a4, doc.a5, doc.a6],
+        [doc.a7, doc.a8, doc.a9]
+    ]
 
-// Calculate row count
-for(r = 0; r < arr.length; r++) {
-    for(c = 0; c < arr[r].length; c++) {
-        sum += arr[r][c];
+    for(r = 0; r < array.length; r++) {
+        magicSum += array[0][r];
+    }
+
+    // Calculate row count
+    for(r = 0; r < array.length; r++) {
+        for(c = 0; c < array[r].length; c++) {
+            sum += array[r][c];
+        }
+        if(sum !== magicSum) isMagic = false;
+        sum = 0;
+    }
+
+    // Calculate column count
+    for(r = 0; r < array.length; r++) {
+        for(c = 0; c < array[r].length; c++) {
+            sum += array[c][r];
+        }
+        if(sum !== magicSum) isMagic = false;
+        sum = 0;
+    }
+
+    // Calculate diagonals count
+    for(i = 0; i < array.length; i++) {    
+        sum += array[i][i];  
     }
     if(sum !== magicSum) isMagic = false;
     sum = 0;
-}
 
-// Calculate column count
-for(r = 0; r < arr.length; r++) {
-    for(c = 0; c < arr[r].length; c++) {
-        sum += arr[c][r];
+
+    // Calculate diagonals count
+    for(i = 0; i < array.length; i++) {    
+        sum += array[i][array.length - i - 1];
     }
     if(sum !== magicSum) isMagic = false;
     sum = 0;
+    
+    if(isMagic){
+        result = "This a magic square";
+    }else{
+        result = "This is not a magic square";
+    }
+    
+    document.getElementById("result").innerHTML = result;
+    
+    magicSum = 0;
+    sum = 0;
+    isMagic = true;
 }
 
-// Calculate diagonals count
-for(i = 0; i < arr.length; i++) {    
-    sum += arr[i][i];  
-}
-if(sum !== magicSum) isMagic = false;
-sum = 0;
 
-
-// Calculate diagonals count
-for(i = 0; i < arr.length; i++) {    
-    sum += arr[i][arr.length - i - 1];
-}
-if(sum !== magicSum) isMagic = false;
-sum = 0;
-
-document.write(`<div style='
-        background:#fff;
-        color:#333;
-        display:inline-block;
-        border-radius:5px;
-        padding:5px 15px;
-        margin:15px;'>` +
-               
-        (function() {
-            if(isMagic){
-                return "This a magic square";
-            }
-
-            return "This is not a magic square" }
-        )() + `</div>`);
+document.write(`<div id='result' style='
+    background:#fff;
+    color:#333;
+    display:inline-block;
+    border-radius:5px;
+    padding:5px 15px;
+    margin:15px;'>Evaluating...</div>`);
